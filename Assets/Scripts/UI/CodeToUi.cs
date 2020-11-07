7ddd2d0ceb967aae3653278ef4869e0f7c152111
louis03015 <louis.beaumont@gmail.com>
2020-11-07 16:04:45 +0100

    Improve UI / UX

--- Assets/Scripts/UI/CodeToUi.cs
+++ Assets/Scripts/UI/CodeToUi.cs
@@ -11,8 +11,8 @@ namespace UI
 {
     public static class CodeToUi
     {
-        private const string SliderTemplatePath = "Prefabs/SliderTemplate";
-        private const string CheckboxTemplatePath = "Prefabs/CheckboxTemplate";
+        private const string SliderTemplatePath = "Prefabs/BlueSliderTemplate";
+        private const string CheckboxTemplatePath = "Prefabs/BlueCheckboxTemplate";
         private static readonly GameObject DefaultSlider = Resources.Load(SliderTemplatePath) as GameObject;
         private static readonly GameObject DefaultCheckBox = Resources.Load(CheckboxTemplatePath) as GameObject;
         
